4c74882c69f81b620a90d867736517f1a3d10948
Francisco Peters <francisco.peters91@gmail.com>
2019-03-13 15:35:10 -0300

    fix. Added calculations for working days
    Now it will calculate the annual return using only the working days between the dates.

--- src/Portfolio.java
+++ src/Portfolio.java
@@ -1,3 +1,4 @@
+import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.time.Year;
 import java.util.ArrayList;
@@ -14,19 +15,30 @@ public class Portfolio {
         if (end.isBefore(begin)) {
             throw new Exception("Insert valid date range");
         }
-        double beginingValue = 0.0;
+        double beginningValue = 0.0;
         double endValue = 0.0;
         for (Stock st : stocks) {
-            beginingValue += st.price(begin);
+            beginningValue += st.price(begin);
             endValue += st.price(end);
         }
-        double profit = endValue - beginingValue;
+        double profit = endValue - beginningValue;
 
-        double overallReturn = profit/beginingValue;
-        long daysBetween = DAYS.between(begin, end);
-        int daysInYear = Year.of(begin.getYear()).length();
+        double overallReturn = profit/beginningValue;
+        long daysBetween = calcWeekDays(begin, end);
+        int daysInYear = 252; // working days
 
         double annualReturn = Math.pow(1 + overallReturn, 1/(daysBetween/daysInYear))-1;
         return annualReturn;
+    }
+
+    public static long calcWeekDays(final LocalDate start, final LocalDate end) {
+        final DayOfWeek startW = start.getDayOfWeek();
+        final DayOfWeek endW = end.getDayOfWeek();
+
+        final long days = DAYS.between(start, end);
+        final long daysWithoutWeekends = days - 2 * ((days + startW.getValue())/7);
+
+        //adjust for starting and ending on a Sunday:
+        return daysWithoutWeekends + (startW == DayOfWeek.SUNDAY ? 1 : 0) + (endW == DayOfWeek.SUNDAY ? 1 : 0);
     }
 }
